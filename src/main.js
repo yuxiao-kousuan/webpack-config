77d4a352c4810b1eddb63ff565d69d3a4f2599c5
yuyu <771466737@qq.com>
2024-11-17 20:05:18 +0800

    基本配置 & eslint校验

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import increase from './js/increase';
 import './css/index.css';
 import './less/index.less';
+import './sass/index.scss';
+import './css/iconfont.css'
 
-console.log(increase(1,2));
+console.log(increase(1, 2));
+
+var a = 1;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require("path")
+const ESLintPlugin = require('eslint-webpack-plugin'); // 所有的插件都需要引入
 
 module.exports = {
     entry: './src/main.js', //入口文件喜欢使用相对路径
     output: {
         // path: path.resolve(__dirname, 'dist') 是指当前文件夹的目录下 新建一个dist文件夹
         path: path.resolve(__dirname, 'dist'), // 这里喜欢使用绝对路径
-        filename: 'main.js'
+        //入口文件打包输出的文件名
+        filename: 'static/js/main.js',
+        clean: true  // 打包前将path目录清空
     },
     // 加载器 用于解析webpack无法解析的文件
     module: {
@@ -14,23 +17,55 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
-                    "style-loader", 
+                    "style-loader",
                     "css-loader",
-                ], //执行顺序从左到右
+                ], //执行顺序从右到左 从下到上 与常理相反
             },
             {
                 test: /\.less$/,
                 use: [
-                    "style-loader", 
+                    "style-loader",
                     "css-loader",
                     "less-loader"
-                ], //执行顺序从左到右
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|web|svg)$/,
+                type: "asset",
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 4 * 1024 // 4kb 
+                    }
+                },
+                generator: {
+                    // hash 文件内容生成唯一id 并制定hash位数, ext: 扩展名， query: url中携带其他参数
+                    filename: "static/images/[hash:10][ext][query]"
+                }
+            },
+            {
+                test: /\.(ttf|woff2?|mp3|mp4|avi)$/, //新资源，希望原封不动输出，直接加上就好
+                type: "asset/resource", // 不进行base64转化 图标不要转化
+                generator: {
+                    // hash 文件内容生成唯一id 并制定hash位数, ext: 扩展名， query: url中携带其他参数
+                    filename: "static/media/[hash:10][ext][query]"
+                }
             }
         ]
     },
     // 插件
     plugins: [
-      // 各种第三方插件 用于辅助开发，常见有热更新等功能
+        new ESLintPlugin({
+            // 检查哪些文件
+            context: path.resolve(__dirname, "src")
+        })
     ],
     //模式
     mode: "development"
